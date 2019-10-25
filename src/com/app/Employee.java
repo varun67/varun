a420b426ec485523423b1a54e1a6be3831a534c5
Varun <Varun@Varun>
2019-10-25 17:51:23 +0530

    2nd change

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -1,5 +1,19 @@
 package com.app;
 
-public class Employee {
+public class Employee 
+{
+	private int i;
+	private double d;
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public double getD() {
+		return d;
+	}
+	
+	
 
 }
